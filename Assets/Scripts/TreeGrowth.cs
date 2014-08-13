47850e642684cbdb14b5895334ff28f3dd6a06b1
anicolette <anicolette@email.arizona.edu>
2014-08-13 09:20:17 -0700

    Created prefab folder, created prefab for Tree, began writing script for growth

---
+++ Assets/Scripts/TreeGrowth.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TreeGrowth : _Mono {
+
+	public float height;
+	public float startingHeight = 3f;
+
+	public float growthRate = .1f;
+
+	// Use this for initialization
+	void Start () {
+		height = startingHeight;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		height += growthRate * Time.deltaTime;
+	}
+}
