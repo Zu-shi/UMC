c08a414121cfe8f02d7eb14feb283db17c0d629a
anicolette <anicolette@email.arizona.edu>
2014-08-14 08:52:03 -0700

    Added GUIText object to height script

--- Assets/Scripts/TreeGrowth.cs
+++ Assets/Scripts/TreeGrowth.cs
@@ -3,18 +3,20 @@ using System.Collections;
 
 public class TreeGrowth : _Mono {
 
-	public float height;
-	public float startingHeight = 3f;
+	public float height = 3f;
 
 	public float growthRate = .1f;
 
+	public GUIText heightText;
+
 	// Use this for initialization
 	void Start () {
-		height = startingHeight;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		height += growthRate * Time.deltaTime;
+
+		heightText.text = "Height: " + height;
 	}
 }
