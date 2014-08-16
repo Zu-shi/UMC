c7126b2df5d7c215bc336442f1307a8ba7d875bb
anicolette <anicolette@email.arizona.edu>
2014-08-15 21:16:44 -0700

    Created abstract Hazard class

---
+++ Assets/Scripts/Hazard.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Hazard : _Mono {
+
+	protected bool hasStarted;
+	protected bool hasFinished;
+	protected bool isHarmful;
+
+	public abstract void Start ();
+
+	public abstract void Finish();
+
+	public abstract void Stop();
+
+
+}
