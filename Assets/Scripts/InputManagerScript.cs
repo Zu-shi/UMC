762105a5a58067162fb2d8b3fd1dfc7ce8eac4d7
Zuoming <Zuoming@Zuomings-MacBook-Air.local>
2014-08-18 11:20:50 -0700

    Added Kinect Scripts

---
+++ Assets/Scripts/InputManagerScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManagerScript : MonoBehaviour {
+
+	public float inputX;
+	public float inputY;
+	public bool mouseMode;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/TreeScene.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TreeScene : MonoBehaviour {
+
+	public GUITimerManager guiTimerManager;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
