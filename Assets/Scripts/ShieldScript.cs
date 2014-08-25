416db020202ae51d47b63658a322fe51d30272e8
anicolette <anicolette@email.arizona.edu>
2014-08-25 09:12:37 -0700

    testPrefab and shieldScript

--- Assets/Scripts/ShieldScript.cs
+++ Assets/Scripts/ShieldScript.cs
@@ -1,15 +1,56 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ShieldScript : MonoBehaviour {
+public class ShieldScript : _Mono {
+
+	public Camera camera;
+
+	public GameObject shield;
+
+	public float parabolaWidth;
+	public float parabolaHeight;
+
+	private float height;
+	private float width;
+
 
 	// Use this for initialization
 	void Start () {
-	
+		height = camera.orthographicSize;
+		width = height * camera.aspect;
+		print (GetYCoord(-600f));
+		print (GetYCoord(-300f));
+		print (GetYCoord(0f));
+		print (GetYCoord(300f));
+		print (GetYCoord(600f));
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		float xPos = inputX;
+		float yPos;
+
+		if(xPos < (-1 * parabolaWidth)){
+			yPos = GetYCoord(-1 * parabolaWidth);
+			xPos = -1 * parabolaWidth;
+		}
+		else if (xPos > parabolaWidth) {
+			yPos = GetYCoord (parabolaWidth);
+			xPos = parabolaWidth;
+		}
+		else {
+			yPos = GetYCoord(xPos);	
+		}
+		shield.transform.position = new Vector3 (xPos, yPos, shield.transform.position.z);
 	}
+
+	float GetYCoord(float xCoord){
+		return GetCoeff() * Mathf.Pow (xCoord, 2f) + parabolaHeight + camera.transform.position.y;
+	}
+
+	float GetCoeff(){
+		return -((height + parabolaHeight) / (Mathf.Pow (parabolaWidth, 2f)));
+	}
+	
 }
