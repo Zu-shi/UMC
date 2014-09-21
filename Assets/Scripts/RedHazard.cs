3a8f0d399388cbe873682bd6943bda1c5e42be3f
anicolette <anicolette@email.arizona.edu>
2014-09-21 08:22:29 -0700

    Bug rotation

--- Assets/Scripts/RedHazard.cs
+++ Assets/Scripts/RedHazard.cs
@@ -6,9 +6,41 @@ public class RedHazard : Hazard {
 
 	public int damage;
 
+	public float radius;
+	public float rotationSpeed;
+
+	private Vector3 center;
+
+
 	// Use this for initialization
 	public override void  Start () {
 		base.Start ();
+		center = transform.position;
+	}
+
+	public override void Update(){
+		if(!isStopped && !hasFinished){
+			float step = speed * Time.deltaTime * Camera.main.orthographicSize;
+			Vector3 cameraPos = Camera.main.transform.position;
+			center = Vector3.MoveTowards(center, 
+			                                         cameraPos - new Vector3(0f, Camera.main.orthographicSize / 4, 0f) , step);
+			z = 0f;
+		}else{
+			float step = -speed * Time.deltaTime * Camera.main.orthographicSize / 6;
+			Vector3 cameraPos = Camera.main.transform.position;
+			center = Vector3.MoveTowards(center, 
+			                                         cameraPos - new Vector3(0f, Camera.main.orthographicSize / 4, 0f) , step);
+			z = 0f;
+		}
+		
+		if( (Globals.stateManager.inCutscene || Globals.stateManager.isGameOver) && !isFading){
+			FadeOut();
+		}
+		float x = center.x + Mathf.Sin (Time.time * rotationSpeed) * radius;
+		float y = center.y + Mathf.Cos (Time.time * rotationSpeed) * radius;
+		transform.position = new Vector3 (x, y, transform.position.z);
+		Debug.Log ("x: " + (x - center.x) + " y: " + (y - center.y));
+
 	}
 	
 	public override void Stop(){
