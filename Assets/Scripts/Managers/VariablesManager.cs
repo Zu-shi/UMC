4698d6db315165185dff3e9dffc921c76bd67394
anicolette <anicolette@email.arizona.edu>
2015-01-22 15:42:53 -0700

    Added system for reading variables

--- Assets/Scripts/Managers/VariablesManager.cs
+++ Assets/Scripts/Managers/VariablesManager.cs
@@ -1,6 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+using System;
+using System.IO;
+
 public class VariablesManager : MonoBehaviour {
 
     //THESE ARE THE DEFAULT VALUES
@@ -9,5 +12,46 @@ public class VariablesManager : MonoBehaviour {
     public static float RoundSpeed = 1f;
     public static int RoundTotalBugs = 15;
     public static float RoundAngleRange = 85f;
+
+	public static string configPath;
+
+	public static void getConfigValues(){
+
+		configPath = Environment.CurrentDirectory + System.IO.Path.DirectorySeparatorChar + "Config.txt";
+
+		System.IO.StreamReader file = new System.IO.StreamReader(configPath);
+
+		string line = "";
+		while ( (line = file.ReadLine) != null ){
+			String[] pair = line.Split(' ');
+
+			switch(pair[0]){
+			case "RoundDuration":
+				RoundDuration = float.Parse(pair[1]);
+				break;
+
+			case "RoundArrivalTime":
+				RoundArrivalTime = float.Parse(pair[1]);
+				break;
+
+			case "RoundSpeed":
+				RoundSpeed = float.Parse(pair[1]);
+				break;
+
+			case "RoundTotalBugs":
+				RoundTotalBugs = int.Parse(pair[1]);
+				break;
+
+			case "RoundAngleRange":
+				RoundAngleRange = float.Parse(pair[1]);
+				break;
+			
+			default:
+				Debug.Log("Config value " + pair[0] + " is unrecognized.");
+				break;
+			}
+		}
+
+	}
 
 }
