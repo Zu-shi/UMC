85d162743acbef4550aa4be10135ba406f1cf495
Zuoming <Zuoming@dhcp-10-132-173-162.uawifi.arizona.edu>
2015-02-19 16:31:31 -0700

    platform dependence

--- Assets/Scripts/Managers/LoggingManager.cs
+++ Assets/Scripts/Managers/LoggingManager.cs
@@ -5,60 +5,71 @@ using System;
 using System.IO;
 
 public class LoggingManager : MonoBehaviour {
-
-	private static string logFilePath;
-	private static int rewardNum = 0;
-
-	public static void startLogging(){
-		//Start writing to log file. Current Directory is used because of issues with permissions.
-		logFilePath = Environment.CurrentDirectory + System.IO.Path.DirectorySeparatorChar + "TreeGameLogFile.txt";
-		
-		String ts = "Game Start: " + TimeStamp ();
-		
-		if (!File.Exists (logFilePath)) {
-			/*using (StreamWriter sw = File.CreateText(logFilePath)){
-				sw.WriteLine(ts);
-			}*/
-		}
-		else{
-			/*using(StreamWriter sw = File.AppendText(logFilePath)){
-				sw.WriteLine(ts);
-			}*/
-		}
-	}
-
-	public static void recordComboCount(){
-        /*
-		int comboNum = Globals.comboManager.comboTally;
-		Debug.Log ("Combo count is " + comboNum);
-
-		using(StreamWriter sw = File.AppendText(logFilePath)){
-			sw.WriteLine("Combo Count: " + comboNum);
-		}
-  */      
-	}
-
-	public static void recordRewards(){
-		/*using(StreamWriter sw = File.AppendText(logFilePath)){
-			sw.WriteLine("Reward Count: " + rewardNum);
-		}*/
-	}
-
-	public static void reward(){
-		rewardNum++;
-	}
-	
-	public static void endLogging(){
-		//Write the end of this game's log entry
-		String ts = "Game End: " + TimeStamp ();
-		/*
-		using(StreamWriter sw = File.AppendText(logFilePath)){
-			
-			sw.WriteLine(ts);
-		}*/
-	}
-	
-	private static string TimeStamp(){
-		return DateTime.Now.ToString ("G");
-	}
+    
+    private static string logFilePath;
+    private static int rewardNum = 0;
+    
+    public static void startLogging(){
+        //Start writing to log file. Current Directory is used because of issues with permissions.
+        
+        #if UNITY_STANDALONE_OSX
+        logFilePath = Environment.CurrentDirectory + System.IO.Path.DirectorySeparatorChar + "TreeGameLogFile.txt";
+        
+        String ts = "Game Start: " + TimeStamp ();
+        
+        if (!File.Exists (logFilePath)) {
+            using (StreamWriter sw = File.CreateText(logFilePath)){
+                sw.WriteLine(ts);
+            }
+        }
+        else{
+            using(StreamWriter sw = File.AppendText(logFilePath)){
+                sw.WriteLine(ts);
+            }
+        }
+        #endif
+    }
+    
+    public static void recordComboCount(){
+        
+        #if UNITY_STANDALONE_OSX
+        int comboNum = Globals.comboManager.comboTally;
+        Debug.Log ("Combo count is " + comboNum);
+        
+        using(StreamWriter sw = File.AppendText(logFilePath)){
+            sw.WriteLine("Combo Count: " + comboNum);
+        }
+        #endif
+        
+    }
+    
+    public static void recordRewards(){
+        
+        #if UNITY_STANDALONE_OSX
+        using(StreamWriter sw = File.AppendText(logFilePath)){
+            sw.WriteLine("Reward Count: " + rewardNum);
+        }
+        #endif
+        
+    }
+    
+    public static void reward(){
+        rewardNum++;
+    }
+    
+    public static void endLogging(){
+        //Write the end of this game's log entry
+        #if UNITY_STANDALONE_OSX
+        String ts = "Game End: " + TimeStamp ();
+        
+        using(StreamWriter sw = File.AppendText(logFilePath)){
+            
+            sw.WriteLine(ts);
+        }
+        #endif
+    }
+    
+    private static string TimeStamp(){
+        return DateTime.Now.ToString ("G");
+    }
 }
