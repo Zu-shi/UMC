f13e23942d002c0dc6b169745e1f97c248a72d7c
Zuoming <Zuoming@Zuomings-MacBook-Air-4.local>
2015-02-20 11:20:56 -0700

    Compiles to iOS and Android phone without modifications

--- Assets/Scripts/Managers/InputManagerScript.cs
+++ Assets/Scripts/Managers/InputManagerScript.cs
@@ -4,78 +4,92 @@ using System.Collections;
 public class InputManagerScript : MonoBehaviour {
     
     public enum ControlMethod { CARTESIAN, POLAR }; 
-
+    
     public float inputX{ get; set; }
     public float inputY{ get; set; }
     public float inputNormX{ get; set; }
     public float inputNormY{ get; set; }
     [Tooltip("Whether to restrict controls to a smaller box in the center.")]
     public bool smallScreenMode;
-	public bool mouseMode;
-	public KinectManager kinectManagerPrefab;
-    public CursorScript handCursor;
+    public bool mouseMode;
+    
+    #if UNITY_IPHONE
+    #elif UNITY_ANDROID
+    #else
+    public KinectManager kinectManagerPrefab;
     private KinectManager kinectManager;
+    #endif
+    
+    public CursorScript handCursor;
     public ControlMethod control = ControlMethod.POLAR;
-
-	// Use this for initialization
-	void Start () {
+    
+    // Use this for initialization
+    void Start () {
         handCursor = GameObject.Find("HandCursor").GetComponent<CursorScript>();
-		inputX = 0;
-		inputY = 0;
-		if(!mouseMode){
-			kinectManager = Object.Instantiate(kinectManagerPrefab, Vector3.zero, Quaternion.identity) as KinectManager;
+        inputX = 0;
+        inputY = 0;
+        #if UNITY_IPHONE
+        #elif UNITY_ANDROID
+        #else
+        if(!mouseMode){
+            kinectManager = Object.Instantiate(kinectManagerPrefab, Vector3.zero, Quaternion.identity) as KinectManager;
             kinectManager.HandCursor1 = handCursor;
-		}
-	}
-
-	public Vector2 inputPos {
-		set {
-			inputX = value.x;
-			inputY = value.y;
-		}
-		get {
-			return new Vector2(inputX, inputY);
-		}
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (mouseMode) {
-			inputX = Camera.main.ScreenToWorldPoint (Input.mousePosition).x;
-			inputY = Camera.main.ScreenToWorldPoint (Input.mousePosition).y;
+        }
+        #endif
+    }
+    
+    public Vector2 inputPos {
+        set {
+            inputX = value.x;
+            inputY = value.y;
+        }
+        get {
+            return new Vector2(inputX, inputY);
+        }
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        if (mouseMode) {
+            inputX = Camera.main.ScreenToWorldPoint (Input.mousePosition).x;
+            inputY = Camera.main.ScreenToWorldPoint (Input.mousePosition).y;
             inputNormX = Input.mousePosition.x/Screen.width;
             inputNormY = Input.mousePosition.y/Screen.height;
             handCursor.x = inputX;
             handCursor.y = inputY;
-		} else {
-			inputX = kinectManager.HandCursor1.x;
-			inputY = kinectManager.HandCursor1.y;
+        } else {
+            #if UNITY_IPHONE
+            #elif UNITY_ANDROID
+            #else
+            inputX = kinectManager.HandCursor1.x;
+            inputY = kinectManager.HandCursor1.y;
             inputNormX = kinectManager.screenPosX;
             inputNormY = kinectManager.screenPosY;
-			// Kinect controls to be implemented.
-		}
-
+            #endif
+            // Kinect controls to be implemented.
+        }
+        
         inputNormX = (smallScreenMode) ? (inputNormX - 0.5f) / 2f + 0.5f : inputNormX;
         inputNormY = (smallScreenMode) ? (inputNormY - 0.5f) / 2f + 0.5f : inputNormY;
-
+        
         if (Input.GetKeyDown (KeyCode.Return)) {  
             //Application.LoadLevel (0);
             Globals.RestartTreeScene(false);
         }  
-		//Debug.Log ("inputX = " + inputX + " inputY = " + inputY);
-	}
-
+        //Debug.Log ("inputX = " + inputX + " inputY = " + inputY);
+    }
+    
     public Vector2 normToScreenPoint (Vector2 v) {
         //Debug.Log("width2:" +v.x * Screen.width + "height2:" +v.y * Screen.height);
         return Camera.main.ScreenToWorldPoint (new Vector2(v.x * Screen.width, v.y * Screen.height));
     }
-
+    
     
     public Vector2 normToScreenPoint (float x, float y) {
         //Debug.Log("width2:" +v.x * Screen.width + "height2:" +v.y * Screen.height);
         return Camera.main.ScreenToWorldPoint (new Vector2(x * Screen.width, y * Screen.height));
     }
-
+    
     public Vector2 screenToNormPoint (float x, float y) {
         //Debug.Log("width2:" +v.x * Screen.width + "height2:" +v.y * Screen.height);
         return new Vector2( (x - Camera.main.transform.position.x) / Utils.halfScreenWidth / 2,
